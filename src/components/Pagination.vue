f4d30a62a2d7106c0e22bfd1dc6c464a7569afb2
jj20375 <desiginerice5908@gmail.com>
2024-07-10 17:12:52 +0800

    fix: defineEmits defineProps withDefaults 問題

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits, defineProps, withDefaults } from "vue";
+import { ref } from "vue";
 interface Props {
     page: number;
     total: number;
